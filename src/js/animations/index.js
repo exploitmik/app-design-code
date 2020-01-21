d2adc73ba6bab7c98d99889e3252812f89c03766
asa <mykeias2_bk@hotmail.com>
2020-01-21 09:16:57 -0300

    fix arrow button and rebuild animations

--- src/js/animations/index.js
+++ src/js/animations/index.js
@@ -3,21 +3,20 @@ export function animationHero(){
 	tl
 		.from('.hero__smartphone', { delay: 1, duration: 2, opacity: 0, x: -80 })
 		.from('.hero__title', { duration: 1, opacity: 0, x: 80 }, "-=1.5")
-		.from('.hero__action', { duration: .5, opacity: 0, y: -20 }, "-=.8")
+		.from('.hero__action', { duration: .5, opacity: 0, y: -20 }, "-=1")
 }
 
 export function animationAbout(){
 	const controller = new ScrollMagic.Controller();
 	const tl = gsap.timeline();
 	tl
-		.from('.workflow__title', { duration: 4, opacity: 0, y: -100 })
-		.from('.workflow__wrapper', { duration: 2, opacity: 0, y: 100, stagger: .8 })
-		.from('.workflow__figure', { duration: 2, opacity: 0, y: -50, stagger: .4})
+		.from('.workflow__title', { duration: 5, opacity: 0, y: 200 })
+		.from('.workflow__wrapper', { duration: 2, opacity: 0, y: 100, stagger: .8 }, "-=2")
 
 	new ScrollMagic.Scene({
 	  triggerElement: ".workflow__content",
 	  duration: "90%",
-	  triggerHook: .8,
+	  triggerHook: .9,
 	  reverse: false,
 	}).setTween(tl).addTo(controller);
 }
@@ -26,12 +25,11 @@ export function animationOffer(){
 	const controller = new ScrollMagic.Controller();
 	const tl = gsap.timeline();
 	tl
-		.from('.offer__wrapper', { duration: 5, opacity: 0, y: -200 })
-		.from('.offer__smartphone', { duration: 2, opacity: 0, scale: 2 }, "-=4")
+		.from('.offer__body', { duration: 1, opacity: 0, y: -200 })
 
 	new ScrollMagic.Scene({
 	  triggerElement: ".offer__content",
-	  duration: "80%",
+	  duration: "100%",
 	  triggerHook: .9,
 	  reverse: false,
 	}).setTween(tl).addTo(controller);
@@ -41,8 +39,8 @@ export function animationFeedback(){
 	const controller = new ScrollMagic.Controller();
 	const tl = gsap.timeline();
 	tl
-		.from('.feedback__title', { duration: 5, opacity: 0, y: 200 })
-		.from('.feedback__description', { duration: 5, opacity: 0, y: 200 })
+		.from('.feedback__title', { duration: 1, opacity: 0, y: 200 })
+		.from('.feedback__description', { duration: 1, opacity: 0, y: 200 }, "-=.4")
 
 	new ScrollMagic.Scene({
 	  triggerElement: ".feedback__content",
